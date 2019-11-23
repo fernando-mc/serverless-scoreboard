85b97a87a5efa2355a1b2f653c9eb2fbad7e74d8
Fernando Medina Corey <fmcorey@gmail.com>
2019-11-23 13:25:28 -0800

    setup handler to refresh recent data

---
+++ handler.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+import requests
+
+def handler(event, context):
+    # Set of Repos to look for metadata
+    frameworks = [
+        'serverless/serverless', 
+        'miserlou/zappa', 
+        'apex/apex', 
+        'aws-amplify/amplify-js', 
+        'awslabs/serverless-application-model', 
+        'mweagle/Sparta',
+        'jorgebastida/gordon',
+    ]
+    # Request data from the GitHub API
+    results = []
+    for framework in frameworks:
+        r = requests.get('https://api.github.com/repos/' + framework)
+        data = json.loads(r.text)
+        framework_stats = {
+            "name": data["name"],
+            "subscribers_count": data["subscribers_count"],
+            "stargazers_count": data["stargazers_count"],
+            "forks_count": data["forks_count"],
+        }
+        results.append(framework_stats)
+    json_result = json.dumps(results)
+    # Output data to S3 file
+    s3 = boto3.client('s3')
+    s3.put_object(
+        ACL='public-read',
+        Body=json_result,
+        Bucket="serverless-scorecard", 
+        Key="recent_results.json",
+    )
+    print("RESULTS:")
+    print(results)
+    return "Success"
